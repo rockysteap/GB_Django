11eab6ac2c611219a5aef8b77431ce7de95bb11a
Ilia Mediankin <i.mediankin@gmail.com>
2024-03-27 10:25:37 +0300

    feat: add lection03

---
+++ lections/myapp3/management/commands/fill_db.py
@@ -0,0 +1,26 @@
+from random import choices
+from django.core.management.base import BaseCommand
+from myapp3.models import Author, Post
+
+LOREM = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+
+
+class Command(BaseCommand):
+    help = "Generate fake authors and posts."
+
+    def add_arguments(self, parser):
+        parser.add_argument('count', type=int, help='User count')
+
+    def handle(self, *args, **kwargs):
+        text = LOREM.split()
+        count = kwargs.get('count')
+        for i in range(1, count + 1):
+            author = Author(name=f'Author_{i}', email=f'mail{i}@mail.ru')
+            author.save()
+            for j in range(1, count + 1):
+                post = Post(
+                    title=f'Title-{j}',
+                    content=" ".join(choices(text, k=64)),
+                    author=author
+                )
+                post.save()
