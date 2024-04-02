9c771cd6e6ebdde88920117a558d6705b194a8db
Ilia Mediankin <i.mediankin@gmail.com>
2024-04-02 12:42:51 +0300

    feat: add lection06

--- lections/lections/settings.py
+++ lections/lections/settings.py
@@ -19,13 +19,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-29jqsh$=esz49p2(+ha2pb0q%xge$8uk(7s8pv39i-0mlwnit_'
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
-    '192.168.88.90',
+    # '192.168.88.90',
+    # '127.0.0.1',
+    'homeworkproject.pythonanywhere.com'
+]
+
+INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
@@ -43,9 +52,12 @@ INSTALLED_APPS = [
     'myapp3',
     'myapp4',
     'myapp5',
+    'myapp6',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -80,10 +92,24 @@ WSGI_APPLICATION = 'lections.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'homeworkproject$default',
+        'USER': 'homeworkproject',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'homeworkproject.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -120,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -180,6 +207,11 @@ LOGGING = {
             'propagate': True,
         },
         'myapp5': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'myapp6': {
             'handlers': ['console', 'file'],
             'level': 'DEBUG',
             'propagate': True,
